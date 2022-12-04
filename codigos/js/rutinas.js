549788014a1e7fb488f9faf7e28df250de713d9f
Jose Fegasu <fegasu@misena.edu.co>
2022-12-04 12:50:12 -0500

    Update rutinas.js

--- codigos/js/rutinas.js
+++ codigos/js/rutinas.js
@@ -1,3 +1,61 @@
-function Ir(){
-    alert("Hola****");
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function Ir(que){
+  location.href=que;
+}
+function Borra(titu,afirma,niega){
+
+
+let a='<center><div class="modal-content" style="width:50%;"><p><b>Desea borrar el registro?<b></br><button onclick=\'Ir("'+afirma+'")\' class="btn btn-outline-primary w3-round-large small">Aceptar</button>&nbsp;<button onclick=\'Ir("'+niega+'")\' class="btn btn-outline-danger w3-round-large small">Cancelar</button><p></div></div></center>';
+document.write(a);
+}
+
+function Confirma(titu,afirma,niega){
+    if(confirm(titu)==true)
+      location.href=afirma;
+      else
+      location.href=niega;
+    }
+
+    function Migas(a){
+      let x='<p>&nbsp;&nbsp;<b><i class="fa fa-bars"></i>';
+        document.write(x);
+        //x="";
+      a.forEach(function(word){
+        let b=word.split('|');
+        if(b[1]!=""){
+        x='&nbsp;&#171;<a style="text-decoration:none;" href="'+b[1]+'">'+b[0]+'</a>&#187;&nbsp;';
+        document.write(x);
+        x="";
+        }else
+        {
+          x='&nbsp;'+b[0]+'&nbsp;</p>';
+        document.write(x);
+        }
+        
+      });
+    }
+  
